47e605f5806228c7ab67176cb1e8bf37c2851e7f
GuillermoMediero <2gdaw01@ARRIAGAINFO.LOCAL>
2022-02-02 09:33:54 +0100

    todo

--- resources/views/operador.blade.php
+++ resources/views/operador.blade.php
@@ -178,7 +178,7 @@
         </div>  
         <div id="añadir" style="position: fixed;top:75vh;left:88vw">
 
-        <a class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+        <a data-bs-toggle="modal" data-bs-target="#staticBackdrop">
             <svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
@@ -189,14 +189,70 @@
 
         
         <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog">
+            <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="staticBackdropLabel">Creacion de credenciales</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        HOLA
+                        <div class="row justify-content-around p-md-3 m-md-2">
+                            <div class="col-12 col-md-7">
+                                <div class="row">
+                                    <div class="col-6">
+                                        <label class="form-label">Nº serie</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                    <div class="col-6">
+                                        <label class="form-label">Direccion</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                    <div class="col-6">
+                                        <label class="form-label">Zona</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                    <div class="col-6">
+                                        <label class="form-label">Modelo</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                    <div class="col-6">
+                                        <label class="form-label">Marca</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                    <div class="col-6">
+                                        <label class="form-label">Segmentos</label>
+                                        <select class="form-select">
+                                            <!--Añadir funion base de datos-->
+                                            <option selected value="Guipuzcoa">Guipuzcoa</option>
+                                            <option selected value="Alava">Alava</option>
+                                            <option selected value="Vizcaya">Vizcaya</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-6">
+                                        <label class="form-label">Tipo Incidencia</label>
+                                        <select class="form-select">
+                                            <!--Añadir funion base de datos-->
+                                            <option selected value="Guipuzcoa">Guipuzcoa</option>
+                                            <option selected value="Alava">Alava</option>
+                                            <option selected value="Vizcaya">Vizcaya</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-6">
+                                        <label class="form-label">Detalles Operador</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                    <div class="col-12 justify-content-center">
+                                        <label class="form-label">Tecnico</label>
+                                        <select class="form-select">
+                                            <!--Añadir funion base de datos-->
+                                            <option selected value="Guipuzcoa">Guipuzcoa</option>
+                                            <option selected value="Alava">Alava</option>
+                                            <option selected value="Vizcaya">Vizcaya</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
