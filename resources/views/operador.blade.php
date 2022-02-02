0ff51b2a965ac8c134aee0586bff195212e10d4c
GuillermoMediero <2gdaw01@ARRIAGAINFO.LOCAL>
2022-02-02 09:41:39 +0100

    todo

--- resources/views/operador.blade.php
+++ resources/views/operador.blade.php
@@ -192,7 +192,7 @@
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="staticBackdropLabel">Creacion de credenciales</h5>
+                        <h5 class="modal-title text-center" id="staticBackdropLabel">Creacion de Incidencias</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
