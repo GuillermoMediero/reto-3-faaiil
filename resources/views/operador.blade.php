40ceee0855247d5c493a2d5b46b3d3a7fd64bb2e
GuillermoMediero <2gdaw01@ARRIAGAINFO.LOCAL>
2022-02-02 12:38:30 +0100

    todo

--- resources/views/operador.blade.php
+++ resources/views/operador.blade.php
@@ -264,17 +264,17 @@
         </div>
 
         <div class="modal fade" id="staticBackdrop2" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog">
+            <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="staticBackdropLabel">Creacion de Incidencias</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" data-bs-toggle="modal" data-bs-target="#staticBackdrop" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
         ...
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Close</button>
                         <button type="button" class="btn btn-primary">Crear Incidencia</button>
                     </div>
                 </div>
