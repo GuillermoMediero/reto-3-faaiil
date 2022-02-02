dfc7302b94a14d9b994a84dada7f917c38b45fb4
GuillermoMediero <2gdaw01@ARRIAGAINFO.LOCAL>
2022-02-02 12:28:11 +0100

    todo

--- resources/views/operador.blade.php
+++ resources/views/operador.blade.php
@@ -257,29 +257,12 @@
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <input type="submit" name="send" value="buscar" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop2" >
+                        <input type="submit" name="send" value="buscar" class="btn btn-primary">
                     </div>
                     </form>
                 </div>
             </div>
         </div>
 
-        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="staticBackdropLabel">Creacion de Incidencias</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-        ...
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Crear Incidencia</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+        
 @endsection
