221fd8128f0e21cfb350b99f2cca584b3e1e8323
GuillermoMediero <2gdaw01@ARRIAGAINFO.LOCAL>
2022-02-02 09:14:05 +0100

    todo

--- resources/views/operador.blade.php
+++ resources/views/operador.blade.php
@@ -178,7 +178,7 @@
         </div>  
         <div id="añadir" style="position: fixed;top:75vh;left:88vw">
 
-        <a data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+        <a class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
             <svg xmlns="http://www.w3.org/2000/svg" width="128" height="128" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
