694ef99a3ed5a5fef90bbf45b5429446a48e5c56
GuillermoMediero <2gdaw01@ARRIAGAINFO.LOCAL>
2022-02-02 12:31:55 +0100

    todo

--- resources/views/operador.blade.php
+++ resources/views/operador.blade.php
@@ -191,8 +191,7 @@
         <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
-                    <form action="" method="post">
-                     @csrf
+
                     <div class="modal-header">
                         <h5 class="modal-title" id="staticBackdropLabel">Creacion de Incidencias</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -257,12 +256,29 @@
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <input type="submit" name="send" value="buscar" class="btn btn-primary">
+                        <input type="submit" name="send" value="buscar" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop2" >
                     </div>
-                    </form>
+            
                 </div>
             </div>
         </div>
 
-        
+        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="staticBackdropLabel">Creacion de Incidencias</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Crear Incidencia</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
