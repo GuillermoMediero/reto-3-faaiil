c944ec1bbdbdad507d3e7b7b56e1d183cb612ba7
GuillermoMediero <2gdaw01@ARRIAGAINFO.LOCAL>
2022-02-02 12:13:30 +0100

    todo

--- resources/views/operador.blade.php
+++ resources/views/operador.blade.php
@@ -191,14 +191,14 @@
         <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
+                    <form action="" method="post">
+                     @csrf
                     <div class="modal-header">
                         <h5 class="modal-title" id="staticBackdropLabel">Creacion de Incidencias</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
                         <div class="row justify-content-around p-md-3 m-md-2">
-                            <form action="" method="post">
-                            @csrf
                             <div class="col-12">
                                 <div class="row">
                                     <div class="col-6 text-center">
@@ -209,6 +209,7 @@
                                         <label class="form-label" for="direccion">Direccion</label>
                                         <input type="text" class="form-control" id="direccion">
                                     </div>
+                                    <!--
                                     <div class="col-6 text-center">
                                         <label class="form-label" for="zona">Zona</label>
                                         <input type="text" class="form-control disabled" id="zona">
@@ -224,7 +225,7 @@
                                     <div class="col-6 text-center">
                                         <label class="form-label" for="segmentos">Segmentos</label>
                                         <select class="form-select" id="segmentos">
-                                            <!--Añadir funion base de datos-->
+                                           
                                             <option selected value="Guipuzcoa">Guipuzcoa</option>
                                             <option selected value="Alava">Alava</option>
                                             <option selected value="Vizcaya">Vizcaya</option>
@@ -233,7 +234,6 @@
                                     <div class="col-6 text-center">
                                         <label class="form-label" for="tipoIncidencia">Tipo Incidencia</label>
                                         <select class="form-select" id="tipoIncidencia">
-                                            <!--Añadir funion base de datos-->
                                             <option selected value="Guipuzcoa">Guipuzcoa</option>
                                             <option selected value="Alava">Alava</option>
                                             <option selected value="Vizcaya">Vizcaya</option>
@@ -242,7 +242,6 @@
                                     <div class="col-6 text-center">
                                         <label class="form-label" for="tecnico">Tecnico</label>
                                         <select class="form-select" id="tecnico">
-                                            <!--Añadir funion base de datos-->
                                             <option selected value="Guipuzcoa">Guipuzcoa</option>
                                             <option selected value="Alava">Alava</option>
                                             <option selected value="Vizcaya">Vizcaya</option>
@@ -251,15 +250,33 @@
                                     <div class="col-12 text-center">
                                         <label class="form-label" for="detallesOp">Detalles Operador</label>
                                         <textarea class="form-control" id="detallesOp" rows="7"></textarea>
-                                    </div>
+                                    </div>-->
                                 </div>
                             </div>
-                            </form>
                         </div>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Understood</button>
+                        <input type="submit" name="send" value="buscar" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop2" data-bs-dismiss="modal">
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="staticBackdropLabel">Creacion de Incidencias</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Close</button>
+                        <button type="button" class="btn btn-primary">Crear Incidencia</button>
                     </div>
                 </div>
             </div>
