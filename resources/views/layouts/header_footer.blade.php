a966e409a61bdd34d5e8d371e95aafed3f626e21
GuillermoMediero <2gdaw01@arriagainfo.local>
2022-02-03 08:54:45 +0100

    todo

--- resources/views/layouts/header_footer.blade.php
+++ resources/views/layouts/header_footer.blade.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="../../css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js"></script>
     <title>Document</title>
 </head>
 <body>
