65e25a4f431f481e3b1340f33170a531efc08514
GuillermoMediero <2gdaw01@ARRIAGAINFO.LOCAL>
2022-02-04 13:04:16 +0100

    todo

--- resources/views/operador.blade.php
+++ resources/views/operador.blade.php
@@ -186,7 +186,12 @@
         </a>
 
         </div>
-
+        
+        <script>
+             $('body').on('hidden.bs.modal', '.modal', function () {
+                $(this).removeData('bs.modal');
+             });
+        </script>
         
         <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered">
