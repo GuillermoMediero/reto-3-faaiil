a9723d119fad88a676d5579e3e6056f9bdb930a0
GuillermoMediero <2gdaw01@ARRIAGAINFO.LOCAL>
2022-02-07 13:40:22 +0100

    todo

--- resources/views/jefe.blade.php
+++ resources/views/jefe.blade.php
@@ -3,9 +3,28 @@
     
     <div class="container"style="height:75vh">
         <h3 class="pt-3">Jefe</h3>
-        @foreach ($incidencias as $incidencia)
-            <h4>Nº : {{ $incidencia->as_serie }}</h4>
-        @endforeach
+        <div class="row">
+            <div class="col-sm-12 col-md-3">
+                <div class="row table-responsive">
+                <div class="col-sm-12">
+                <table class="table table-bordered border-primary table-success table-striped">
+                    <thead class="table-dark text-white">
+                    <th>Tipo Incidencia</th>
+                    <th>Nº Resuelto</th>
+                    </thead>
+                    @foreach ($incidencias as $incidencia )
+                    <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $incidencia->tipo }}</td>
+                    <td>{{ $incidencia->estado == 2}}</td>
+                    </tr>
+                    @endforeach
+                </table>
+                </div>
+                </div> 
+            </div> 
+
+        </div>
     </div>
     
 @endsection
