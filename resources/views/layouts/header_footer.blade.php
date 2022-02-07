e086cbdd7f1f0782ebce9bc6cae9006891cede12
GuillermoMediero <2gdaw01@ARRIAGAINFO.LOCAL>
2022-02-07 10:43:35 +0100

    todo

--- resources/views/layouts/header_footer.blade.php
+++ resources/views/layouts/header_footer.blade.php
@@ -32,20 +32,6 @@
                 <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
               </svg>
             </a>
-            @else
-            <a class="dropdown" href="#" id="perfil" data-bs-toggle="dropdown" aria-expanded="false">
-              <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
-              </svg>
-            </a>
-            @endif
-          
-            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="perfil">
-              <li class="dropdown-item active">Nombre</li>
-              <li><a class="dropdown-item" href="#">Perfil</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item text-danger" href="{{ route('logout') }}">Cerrar Session</a></li>
-            </ul>
 
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="perfilAdmin">
               <li class="dropdown-item active">Nombre</li>
@@ -56,6 +42,20 @@
               <li><hr class="dropdown-divider"></li>
               <li><a class="dropdown-item text-danger" href="{{ route('logout') }}">Cerrar Session</a></li>
             </ul>
+            @else
+            <a class="dropdown" href="#" id="perfil" data-bs-toggle="dropdown" aria-expanded="false">
+              <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+              </svg>
+            </a>
+
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="perfil">
+              <li class="dropdown-item active">Nombre</li>
+              <li><a class="dropdown-item" href="#">Perfil</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item text-danger" href="{{ route('logout') }}">Cerrar Session</a></li>
+            </ul>
+            @endif
           </div>
 			  </nav>
         </div>
@@ -186,14 +186,29 @@
           <footer class="row bg-dark text-center text-white">
             <nav class="navbar navbar-dark bg-dark">
               <div class="text-center p-1 ">
-                
-                <a class="nav-link text-white" href="#">Contacte al jefe</a>
+                @if(Auth::user()->rol=="Tecnico")
+                <a class="nav-link text-white" href="#">Contacte al Operario</a>
+                @elseif(Auth::user()->rol=="Operador")
+                  <a class="nav-link text-white" href="#">Contacte al Jefe</a>
+                @elseif(Auth::user()->rol=="Jefe")
+                <a class="nav-link text-white" href="#">Contacte a Julen</a>
+                @endif
               </div>
               <div  class="text-center bg- p-1 ">
                 <a class="nav-link text-white" href="">© 2022 Creative Commons: Igobide <img alt="Licencia Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a>
               </div>
               <div class="text-center p-1">
+                @if()
+                <a class="nav-link text-white dropup" href="#"  id="perfilAdmin" data-bs-toggle="dropdown" aria-expanded="false">Manual de la aplicacion</a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Manual Tecnico</a></li>
+                  <li><a class="dropdown-item" href="#">Manual Operario</a></li>
+                  <li><a class="dropdown-item" href="#">Manual Jefe</a></li>
+                  <li><a class="dropdown-item" href="#">Manual Julen</a></li>
+                </ul>
+                @else
                 <a class="nav-link text-white" href="#">Manual de la aplicacion</a>
+                @endif
               </div>
              </nav>
 
