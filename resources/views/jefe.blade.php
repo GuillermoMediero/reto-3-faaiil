ca23468e39cdb55a7a0e58a526f6f7639e3c7ddb
GuillermoMediero <2gdaw01@ARRIAGAINFO.LOCAL>
2022-02-07 14:10:00 +0100

    todo

--- resources/views/jefe.blade.php
+++ resources/views/jefe.blade.php
@@ -4,26 +4,62 @@
     <div class="container"style="height:75vh">
         <h3 class="pt-3">Jefe</h3>
         <div class="row">
-            <div class="col-sm-12 col-md-3">
+            <div class="col-sm-12 col-md-6">
                 <div class="row table-responsive">
                 <div class="col-sm-12">
                 <table class="table table-bordered border-primary table-success table-striped">
                     <thead class="table-dark text-white">
-                    <th>id Incidencia</th>  
                     <th>Tipo Incidencia</th>
                     <th>Nº Resuelto</th>
                     </thead>
                     @foreach ($incidencias as $incidencia )
                     <tr>
                     <td>{{ $incidencia->tipo }}</td>
-                    <td>{{ $incidencia->estado == 2}}{{$loop->count}}</td>
+                    <td>{{ $incidencia->estado == 2}</td>
                     </tr>
                     @endforeach
                 </table>
                 </div>
                 </div> 
             </div> 
-
+            <!--Tabla-->
+            <div class="col-sm-12 col-md-6">
+                <div class="row table-responsive">
+                <div class="col-sm-12">
+                <table class="table table-bordered border-primary table-success table-striped">
+                    <thead class="table-dark text-white">
+                    <th>Tipo Incidencia</th>
+                    <th>Nº Resuelto</th>
+                    </thead>
+                    @foreach ($incidencias as $incidencia )
+                    <tr>
+                    <td>{{ $incidencia->tipo }}</td>
+                    <td>{{ $incidencia->estado == 2}</td>
+                    </tr>
+                    @endforeach
+                </table>
+                </div>
+                </div> 
+            </div> 
+            <!--tabla-->
+            <div class="col-sm-12 col-md-6">
+                <div class="row table-responsive">
+                <div class="col-sm-12">
+                <table class="table table-bordered border-primary table-success table-striped">
+                    <thead class="table-dark text-white">
+                    <th>Tipo Incidencia</th>
+                    <th>Nº Resuelto</th>
+                    </thead>
+                    @foreach ($incidencias as $incidencia )
+                    <tr>
+                    <td>{{ $incidencia->tipo }}</td>
+                    <td>{{ $incidencia->estado == 2}</td>
+                    </tr>
+                    @endforeach
+                </table>
+                </div>
+                </div> 
+            </div> 
         </div>
     </div>
     
